cc372a0129b5286516d5a0dd3b6c90f8c7d6a589
Mason Hollis <masondhollis@gmail.com>
2016-12-27 10:07:06 -0600

    Initial Set Up

---
+++ js/app.js
@@ -0,0 +1,44 @@
+angular.module('myApp', ['ngAnimate','ui.router'])
+  .config(['$stateProvider','$urlRouterProvider','$locationProvider',function($stateProvider,$urlRouterProvider,$locationProvider){
+
+
+
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+
+    $stateProvider.state('home',{
+        url:'/',
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
+    });
+
+    $stateProvider.state('products', {
+        url: '/products',
+        templateUrl: 'partials/products.html',
+        controller: 'ProductsCtrl'
+    });
+
+    $stateProvider.state('about', {
+      url: '/about',
+      templateUrl: 'partials/about.html',
+      controller: 'AboutCtrl'
+    });
+
+    $stateProvider.state('recipes', {
+      url: '/recipes',
+      templateUrl: 'partials/recipes.html',
+      controller: 'RecipesCtrl'
+    });
+
+    $stateProvider.state('purchase', {
+      url: '/purchase/:waffle',
+      templateUrl: 'partials/purchase.html',
+      controller: 'PurchaseCtrl'
+    });
+
+
+    //$urlRouterProvider.otherwise('/');
+
+}]);
+
+var app =angular.module('myApp');
---
+++ js/controllers.js
@@ -0,0 +1,106 @@
+app.controller('MainCtrl', function($scope, $rootScope, $window, $http, $location) {
+
+  //ANIMATIONS
+  $scope.aboutClicked = function() {
+      if (window.location.pathname == "/") {
+          $('html, body').animate({
+              scrollTop: $("#about").offset().top
+          }, 500);
+      }
+      else {
+          setTimeout(function(){
+              $('html, body').animate({
+                  scrollTop: $("#about").offset().top
+              }, 500);
+          }, 10);
+      }
+  };
+  $scope.projectsClicked = function() {
+      if (window.location.pathname == "/") {
+          $('html, body').animate({
+              scrollTop: $("html").offset().top
+          }, 200);
+      }
+      else {
+          setTimeout(function(){
+              $('html, body').animate({
+                  scrollTop: $("html").offset().top
+              }, 200);
+          }, 10);
+      }
+  };
+  $scope.contactClicked = function() {
+      if (window.location.pathname == "/contact") {
+          $('html, body').animate({
+              scrollTop: $("html").offset().top
+          }, 200);
+      }
+      else {
+          setTimeout(function(){
+              $('html, body').animate({
+                  scrollTop: $("html").offset().top
+              }, 200);
+          }, 10);
+      }
+  };
+
+})
+
+
+app.controller('HomeCtrl', function($scope, $rootScope, $http, $location) {
+
+
+})
+
+app.controller('ProductsCtrl', function($scope, $rootScope, $http, $location) {
+
+    // GET WAFFLE INFO
+    $http({
+      method: "GET",
+      url: "/json/waffles.json",
+    }).then(function successCallback(response) {
+
+      //This is used in this partial/controller
+      $scope.waffles = response.data.waffle_types;
+
+      //This saves it to use in other partials so we dont have to make this call again
+      $rootScope.waffles = response.data.waffle_types;
+    });
+
+    // GO TO TRANSACTION PAGE ON PRODUCT CLICK
+    $scope.waffleClicked = function(waffle) {
+        $location.url("purchase/"+waffle.urlextension);
+    }
+})
+
+app.controller('AboutCtrl', function($scope, $rootScope) {
+
+})
+
+app.controller('RecipesCtrl', function($scope, $rootScope) {
+
+})
+
+app.controller('PurchaseCtrl', function ($scope, $rootScope, $http, $window, $location) {
+    //Get query string from URL
+    var qs = $window.location.pathname.split('/')[2];
+
+    $http({
+      method: "GET",
+      url: "/json/waffles.json",
+      cache: false,
+    }).then(function successCallback(response) {
+
+      var waffles = response.data.waffle_types;
+      for (i in waffles) {
+
+        if (qs == waffles[i].urlextension) {
+          $scope.waffle = response.data.waffle_types[i];
+          $scope.header_title = $scope.waffle.name;
+        }
+      }
+      if (!$scope.waffle) {
+        $location.url('/');
+      }
+    });
+})
